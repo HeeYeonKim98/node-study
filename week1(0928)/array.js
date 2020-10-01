733cc55cb71b3b541b738dbd2e65072b58145efc
HeeYeonKim98 <wow9896@naver.com>
2020-10-01 23:56:24 +0900

    first commit

---
+++ week1(0928)/array.js
@@ -0,0 +1,14 @@
+const arr1 = [];
+const arr2 = [];
+
+const arr3 = [1, 2, 3, 4, 5, 6, 7];
+arr3.forEach((el) => console.log(el + 10));
+//forEach는 break와 continue 없기 때문에 잘 쓰지 않음.
+//some은 false-continue true-break로 사용함.
+
+//앞으로는 for문 while문을 사용하지 않고 some이나 제공해주는 api를 활용하여 서버를 구성한다.
+
+//map filter slice splice api 사용 능숙하게 연습하기
+//깊은 복사, 얕은 복사 능숙하게 연습하기
+//스프레드 연산자
+//호이스팅

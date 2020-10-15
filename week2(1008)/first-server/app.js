d98b5d899851862d5023a9582085e3418617d3e5
HeeYeonKim98 <wow9896@naver.com>
2020-10-15 18:21:49 +0900

    Complete : week2

--- week2(1008)/first-server/app.js
+++ week2(1008)/first-server/app.js
@@ -6,32 +6,32 @@ const app = express();
 //router
 // HTTP GET 방식
 app.get("/", (req, res, next) => {
-  res.send("<h1>hello</h1>");
+    res.send("<h1>hello</h1>");
 });
 
 //res.render : 조각조각들을 하나의 html로 만들어줌
 app.get("/users", (req, res) => {
-  res.send("<h1>users</h1>");
+    res.send("<h1>users</h1>");
 });
 
 app.get("/api", (req, res) => {
-  res.json({ name: "heeyeon", success: true });
+    res.json({ name: "heeyeon", success: true });
 });
 
 //클라이언트가 요청
 app.get("/param/:id", (req, res) => {
-  const { id } = req.params;
-  console.log(id);
-  res.end();
+    const { id } = req.params;
+    console.log(id);
+    res.end();
 });
 
 app.get("/query", (req, res) => {
-  const { name } = req.query; //구조 분해 할당
-  console.log(name);
-  res.end();
+    const { name } = req.query; //구조 분해 할당
+    console.log(name);
+    res.end();
 });
 
 // port binding
 app.listen(3001, () => {
-  console.log("http://localhost:3001");
+    console.log("http://localhost:3001");
 });

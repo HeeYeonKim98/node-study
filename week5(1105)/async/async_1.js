87bc0aa90f56be3a554985928961053a520f418e
HeeYeonKim98 <wow9896@naver.com>
2020-11-06 21:46:34 +0900

    update : week5

--- week5(1105)/async/async_1.js
+++ week5(1105)/async/async_1.js
@@ -1,13 +1,11 @@
 const fs = require("fs");
 
-// function callback(err, data) {
-//     if (err) console.log(err);
-//     else console.log(data);
-// }
+function callback(err, data) {
+    if (err) console.log(err);
+    else console.log(data);
+}
 
-const res = fs.readFileSync("./tt.txt", { encoding: "utf8" });
+const res = fs.readFileSync("./tt.txt", { encoding: "utf8" }, callback);
 
-console.log("1");
 console.log("1");
 console.log(res);
-//콜 스택 부터 (메인 스레드) 적용하고 넘어감
